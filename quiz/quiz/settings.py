707f1e8cca96fdeac5f8edf182c7be092cacbf8d
Ivan <ivanalmin1997@mail.ru>
2021-11-23 17:03:05 +0300

    reworked admin panel (again)

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -121,4 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_URL = '/login/'
--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -16,14 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import url
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view()),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
+
