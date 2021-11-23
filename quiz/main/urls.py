21c8d6702619be865957c9473742cb83a2da24a7
Ivan <ivanalmin1997@mail.ru>
2021-11-23 17:47:41 +0300

    Modified links and urls also reworked html templates

--- quiz/main/urls.py
+++ quiz/main/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from django.conf.urls import url
 
 urlpatterns = [
-    path('get_quiz/', views.QuizListView.as_view()),
-    url(r'^pass_quiz/(?P<name>[\w]+)/$', views.PassQuiz.as_view()),
-    url(r'^pass_quiz/(?P<name>[\w]+)/anon/$', views.PassQuizAnon.as_view()),
-    path('get_complited_quiz/', views.GetComplitedQuizes.as_view()),
-    path('', views.GetStartPage.as_view(), )
+    path('get_quiz/', views.QuizListView.as_view(), name='quiz_getter'),
+    url(r'^pass_quiz/(?P<name>[\w]+)/$', views.PassQuiz.as_view(), name='quiz_passer'),
+    url(r'^pass_quiz/(?P<name>[\w]+)/anon/$', views.PassQuizAnon.as_view(), name='anon_quiz_passer'),
+    path('get_complited_quiz/', views.GetComplitedQuizes.as_view(), name='complete_quiz_getter'),
+    path('', views.GetStartPage.as_view(), name='start_page')
 ]
