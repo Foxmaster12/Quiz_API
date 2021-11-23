27e84a030e12b555286a85becb759ba79ce98a34
Ivan <ivanalmin1997@mail.ru>
2021-11-23 15:36:11 +0300

    Fixed admin panel working

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view()),
     path('main/', include('main.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
